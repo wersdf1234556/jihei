b8aa67dff1a889043fdf6a05a3928110c84f1d28
tonzoc <wangxu.tonzoc@aliyun.com>
2021-03-25 14:49:14 +0800

    bug fix

--- src/main/java/org/tonzoc/mapper/LabPmsSpringbackMapper.java
+++ src/main/java/org/tonzoc/mapper/LabPmsSpringbackMapper.java
@@ -9,15 +9,15 @@ import java.util.List;
 public interface LabPmsSpringbackMapper extends BaseMapper<LabPmsSpringbackModel> {
     @Select("select tenders.guid                     as tenderGuid,\n" +
             "       tenders.name                     as tenderName,\n" +
-            "       isnull(labPmsSpringback.num, 0) as num\n" +
+            "       isnull(labPmsSpringbacks.num, 0) as num\n" +
             "from labTenders\n" +
             "         left join tenders\n" +
             "                   on tenders.guid = labTenders.tenderGuid\n" +
             "         left join\n" +
             "     (select sectionId, count(1) as num\n" +
-            "      from labPmsSpringback\n" +
-            "      group by sectionId) as labPmsSpringback\n" +
-            "     on labTenders.mappingTenderGuid = labPmsSpringback.sectionId\n" +
+            "      from labPmsSpringbacks\n" +
+            "      group by sectionId) as labPmsSpringbacks\n" +
+            "     on labTenders.mappingTenderGuid = labPmsSpringbacks.sectionId\n" +
             "order by tenders.sortId")
     List<LabStatModel> listStatistics ();
 }
--- src/main/java/org/tonzoc/mapper/LabSofteningPointMapper.java
+++ src/main/java/org/tonzoc/mapper/LabSofteningPointMapper.java
@@ -12,15 +12,15 @@ import java.util.List;
 public interface LabSofteningPointMapper extends BaseMapper<LabSofteningPointModel> {
     @Select("select tenders.guid                     as tenderGuid,\n" +
             "       tenders.name                     as tenderName,\n" +
-            "       isnull(labSofteningPoint.num, 0) as num\n" +
+            "       isnull(labSofteningPoints.num, 0) as num\n" +
             "from labTenders\n" +
             "         left join tenders\n" +
             "                   on tenders.guid = labTenders.tenderGuid\n" +
             "         left join\n" +
             "     (select sectionId, count(1) as num\n" +
-            "      from labSofteningPoint\n" +
-            "      group by sectionId) as labSofteningPoint\n" +
-            "     on labTenders.mappingTenderGuid = labSofteningPoint.sectionId\n" +
+            "      from labSofteningPoints\n" +
+            "      group by sectionId) as labSofteningPoints\n" +
+            "     on labTenders.mappingTenderGuid = labSofteningPoints.sectionId\n" +
             "order by tenders.sortId")
     List<LabStatModel> listStatistics ();
 }
