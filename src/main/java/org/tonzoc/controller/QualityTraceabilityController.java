4514895bd5e72057f39cab9ced5851c21f3ed3d6
李先生 <736318704@qq.com>
2021-04-02 08:49:17 +0800

    修改智慧监理条件查询

--- src/main/java/org/tonzoc/controller/QualityTraceabilityController.java
+++ src/main/java/org/tonzoc/controller/QualityTraceabilityController.java
@@ -50,12 +50,12 @@ public class QualityTraceabilityController extends BaseController {
         if (accounType != null) {
             if (accounType.equals("2") && "0".equals(flag)){
                 // flag = 0 施工单位查到未提交，监理查不到
-                if (qualityTraceabilityQueryParams.getStatus() == null || "".equals(qualityTraceabilityQueryParams)){
+                if (qualityTraceabilityQueryParams.getStatus() == null || "".equals(qualityTraceabilityQueryParams.getStatus())){
                     qualityTraceabilityQueryParams.setStatus("submitted,unFinish,finish");
                 }
                 qualityTraceabilityQueryParams.setCurrentTenderGuid(currentTenderGuid);
             }else if (accounType.equals("5") && "0".equals(flag)) {
-                if (qualityTraceabilityQueryParams.getStatus() == null || "".equals(qualityTraceabilityQueryParams)){
+                if (qualityTraceabilityQueryParams.getStatus() == null || "".equals(qualityTraceabilityQueryParams.getStatus())){
                     qualityTraceabilityQueryParams.setStatus("unFinish,finish");
                 }
                 qualityTraceabilityQueryParams.setCurrentTenderGuid(currentTenderGuid);
