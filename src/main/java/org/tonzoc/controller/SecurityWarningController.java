dc2d29f6d5bfe138f1eb2e99e6692dbdf2402c42
tonzoc <wangxu.tonzoc@aliyun.com>
2021-04-18 18:05:11 +0800

    hhhll

--- src/main/java/org/tonzoc/controller/SecurityWarningController.java
+++ src/main/java/org/tonzoc/controller/SecurityWarningController.java
@@ -3,17 +3,25 @@ package org.tonzoc.controller;
 import com.github.pagehelper.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.tonzoc.configuration.IntelliSiteProperties;
 import org.tonzoc.controller.params.PageQueryParams;
 import org.tonzoc.controller.params.SecurityWarningQueryParams;
 import org.tonzoc.controller.response.PageResponse;
 import org.tonzoc.exception.PageException;
+import org.tonzoc.model.AttachmentModel;
 import org.tonzoc.model.SecurityWarningModel;
+import org.tonzoc.service.IAttachmentService;
+import org.tonzoc.service.IAttendanceService;
 import org.tonzoc.service.ISecurityWarningService;
 import org.tonzoc.support.param.SqlQueryParam;
+import sun.misc.BASE64Decoder;
 
 import javax.validation.Valid;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "securityWarning")
@@ -21,6 +29,10 @@ public class SecurityWarningController extends BaseController {
 
     @Autowired
     private ISecurityWarningService securityWarningService;
+    @Autowired
+    private IntelliSiteProperties intelliSiteProperties;
+    @Autowired
+    private IAttachmentService attachmentService;
 
     @GetMapping
     public PageResponse list(PageQueryParams pageQueryParams, SecurityWarningQueryParams securityWarningQueryParams)
@@ -37,7 +49,16 @@ public class SecurityWarningController extends BaseController {
 
     @PostMapping
     public void add(@RequestBody @Valid SecurityWarningModel securityWarningModel) {
-        this.securityWarningService.save(securityWarningModel);
+        String imageData = securityWarningModel.getImage();
+        String filePath = intelliSiteProperties.getFilePath() + "/securityWarningImages/" + UUID.randomUUID().toString() + ".jpg";
+        if (GenerateImage(imageData, filePath)) {
+            AttachmentModel attachmentModel = new AttachmentModel();
+            attachmentModel.setUrl(filePath);
+            this.attachmentService.save(attachmentModel);
+            securityWarningModel.setImage(attachmentModel.getGuid());
+            this.securityWarningService.save(securityWarningModel);
+        }
+
     }
 
     @PutMapping(value = "{guid}")
@@ -53,5 +74,31 @@ public class SecurityWarningController extends BaseController {
     @PostMapping(value = "removeMany")
     public void removeMany(String guids) throws Exception {
         securityWarningService.removeMany(guids);
+    }
+
+    private static boolean GenerateImage(String imgStr, String filePath) {
+        if (imgStr == null) {
+            // 图像数据为空
+            return false;
+        }
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            // Base64解码
+            byte[] b = decoder.decodeBuffer(imgStr);
+            for (int i = 0; i < b.length; ++i) {
+                if (b[i] < 0) {// 调整异常数据
+                    b[i] += 256;
+                }
+            }
+            // 生成jpeg图片
+            String imgFilePath = filePath;// 新生成的图片
+            OutputStream out = new FileOutputStream(imgFilePath);
+            out.write(b);
+            out.flush();
+            out.close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
--- src/main/java/org/tonzoc/controller/params/SecurityWarningQueryParams.java
+++ src/main/java/org/tonzoc/controller/params/SecurityWarningQueryParams.java
@@ -3,5 +3,26 @@ package org.tonzoc.controller.params;
 import org.tonzoc.annotation.Operator;
 
 public class SecurityWarningQueryParams {
+    @Operator(value = "gte", field = "datetime")
+    private String startDate;
 
+    @Operator(value = "lte", field = "datetime")
+    private String endDate;
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        System.out.println(startDate);
+        this.startDate = (startDate.equals("") ? "1900-01-01" : startDate) + " 00:00:00";
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = (endDate.equals("") ? "2090-12-31" : endDate) + " 23:59:59";
+    }
 }
