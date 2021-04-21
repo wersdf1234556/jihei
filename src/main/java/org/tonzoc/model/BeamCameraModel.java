a5850ee0a8d6d4657b481cc46f4641fe95fef9b7
李先生 <736318704@qq.com>
2021-04-21 16:15:04 +0800

    新增梁场摄像头

---
+++ src/main/java/org/tonzoc/model/BeamCameraModel.java
@@ -0,0 +1,95 @@
+package org.tonzoc.model;
+
+import org.tonzoc.annotation.*;
+
+// 梁场摄像头
+@Table("BeamCameras")
+public class BeamCameraModel extends BaseModel{
+
+    @NotInsertColumn
+    @PrimaryKey
+    @Column(value = "guid")
+    private String guid;
+    @Column(value = "name")
+    private String name;
+    @Column(value = "deviceSerial")
+    private String deviceSerial;
+    @Column(value = "chanelNo")
+    private String chanelNo;
+    @Column(value = "tenderGuid")
+    private String tenderGuid;
+    @Column(value = "sortId")
+    private Integer sortId;
+    @Column(value = "typeGuid")
+    private String typeGuid;
+
+    @JoinColumn(value = "name", type = TenderModel.class, leftColumn = "typeGuid", rightColumn = "guid")
+    private String tenderName;
+
+    public BeamCameraModel() {
+    }
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeviceSerial() {
+        return deviceSerial;
+    }
+
+    public void setDeviceSerial(String deviceSerial) {
+        this.deviceSerial = deviceSerial;
+    }
+
+    public String getChanelNo() {
+        return chanelNo;
+    }
+
+    public void setChanelNo(String chanelNo) {
+        this.chanelNo = chanelNo;
+    }
+
+    public String getTenderGuid() {
+        return tenderGuid;
+    }
+
+    public void setTenderGuid(String tenderGuid) {
+        this.tenderGuid = tenderGuid;
+    }
+
+    public Integer getSortId() {
+        return sortId;
+    }
+
+    public void setSortId(Integer sortId) {
+        this.sortId = sortId;
+    }
+
+    public String getTypeGuid() {
+        return typeGuid;
+    }
+
+    public void setTypeGuid(String typeGuid) {
+        this.typeGuid = typeGuid;
+    }
+
+    public String getTenderName() {
+        return tenderName;
+    }
+
+    public void setTenderName(String tenderName) {
+        this.tenderName = tenderName;
+    }
+}
