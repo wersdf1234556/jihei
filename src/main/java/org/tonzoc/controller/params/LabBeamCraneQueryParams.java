d29447945f722c397c77f6596b4a60c0e9026d3e
李先生 <736318704@qq.com>
2021-05-18 09:22:15 +0800

    add beamCamera select tenderGuid

--- src/main/java/org/tonzoc/controller/params/LabBeamCraneQueryParams.java
+++ src/main/java/org/tonzoc/controller/params/LabBeamCraneQueryParams.java
@@ -4,4 +4,34 @@ import org.tonzoc.annotation.Operator;
 
 public class LabBeamCraneQueryParams {
 
+    @Operator(value = "eq", field = "guid")
+    private String guid;
+    @Operator(value = "eq", field = "tenderGuid")
+    private String tenderGuid;
+    @Operator(value = "eq", field = "equipmentName")
+    private String equipmentName;
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getTenderGuid() {
+        return tenderGuid;
+    }
+
+    public void setTenderGuid(String tenderGuid) {
+        this.tenderGuid = tenderGuid;
+    }
+
+    public String getEquipmentName() {
+        return equipmentName;
+    }
+
+    public void setEquipmentName(String equipmentName) {
+        this.equipmentName = equipmentName;
+    }
 }
